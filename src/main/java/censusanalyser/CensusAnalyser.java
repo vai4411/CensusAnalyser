c8aed2d79082dbae1f39fdcdf7550c019fd7684e
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-04-30 13:05:36 +0530

    REFACTORED: Dry Principle Is Used

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -14,11 +14,7 @@ public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(IndiaCensusCSV.class);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+            Iterator<IndiaCensusCSV> censusCSVIterator = getCSVIterator(reader,IndiaCensusCSV.class);
             Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
             int numOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
             return numOfEateries;
@@ -30,16 +26,20 @@ public class CensusAnalyser {
     public int loadIndianStatesCode(String csvFilePath) throws CensusAnalyserException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-            CsvToBeanBuilder<IndianStatesCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(IndianStatesCSV.class);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndianStatesCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndianStatesCSV> censusCSVIterator = csvToBean.iterator();
+            Iterator<IndianStatesCSV> censusCSVIterator = getCSVIterator(reader,IndianStatesCSV.class);
             Iterable<IndianStatesCSV> csvIterable = () -> censusCSVIterator;
             int numOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
             return numOfEateries;
         } catch (IOException e) {
             throw new CensusAnalyserException(TestException.States.getException());
         }
+    }
+
+    public <T>Iterator<T> getCSVIterator(Readable reader, Class<T> csvClass) {
+        CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder<T>((Reader) reader);
+        csvToBeanBuilder.withType(csvClass);
+        csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+        CsvToBean<T> csvToBean = csvToBeanBuilder.build();
+        return csvToBean.iterator();
     }
 }
